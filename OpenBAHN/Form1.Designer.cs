fd67db05c388de9f504c97e5cc698501bc5f0f27
jakubg1 <MASLAKMAX@Wp.pl>
2017-06-20 20:59:31 +0200

    init0
    Falstart

---
+++ OpenBAHN/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace OpenBAHN
+{
+    partial class Layout
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Grid = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Grid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Grid
+            // 
+            this.Grid.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.Grid.Image = global::OpenBAHN.Properties.Resources.id0;
+            this.Grid.Location = new System.Drawing.Point(0, 0);
+            this.Grid.Name = "Grid";
+            this.Grid.Size = new System.Drawing.Size(40, 60);
+            this.Grid.TabIndex = 0;
+            this.Grid.TabStop = false;
+            // 
+            // Layout
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(1008, 730);
+            this.Controls.Add(this.Grid);
+            this.Name = "Layout";
+            this.Text = "Layout";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.Grid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Grid;
+    }
+}
+
---
+++ OpenBAHN/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace OpenBAHN
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Layout());
+        }
+        static void WriteToList(int x, int y, int id, bool haveParam, string param1, int param2, bool param3)
+        {
+            // *** Kod by Nitro modyfikacja i dostosowanie by jakubg1 ***
+            MojaKlasa tmk = new MojaKlasa();
+
+            //nowy item
+            //definiuje nowy obiekt MojaKlasa zanim dodam do listy obiektow 
+            tmk = new MojaKlasa();
+            tmk.x_kratki = x;
+            tmk.y_kratki = y;
+            tmk.id = id;
+            tmk.maParametry = haveParam;
+            if (haveParam)
+            {
+                tmk.parametry.parametr1 = param1;
+                tmk.parametry.parametr2 = param2;
+                tmk.parametry.parametr3 = param3;
+            }
+            //dodaje obiekt do listy
+            mk.Add(tmk);
+        }
+        static void ReadFromList(int itemNumber)
+        {
+            /*//wyswietl
+            for (int i = 0; i < mk.Count; i++)
+            {
+                Console.WriteLine("Item nr: " + i);
+                Console.WriteLine("Kratka [x,y]: [" + mk[i].x_kratki + "," + mk[i].y_kratki + "]");
+                Console.WriteLine("ID: " + mk[i].id.ToString());
+
+                //ponizej mozna pominac i walic if sprawdzajac czy ma wartosc inna od pustej
+                // if(mk[i].parametry.parametr1 != "")
+                // lub if(!String.IsNullOrEmpty(mk[i].parametry.parametr1)) bo sprawdzamy wartosc stringa
+                Console.WriteLine("Ma parametry?: " + mk[i].maParametry.ToString());
+
+                if (mk[i].maParametry)
+                {
+                    //Console.WriteLine("Ma parametry wiec je wyswietlmy");
+                    Console.WriteLine("parametr1: " + mk[i].parametry.parametr1);
+                    Console.WriteLine("parametr2: " + mk[i].parametry.parametr2);
+                    Console.WriteLine("parametr3: " + mk[i].parametry.parametr3);
+                }
+
+                Console.WriteLine("================");
+            }
+
+
+
+            Console.ReadKey();
+            */
+            //Console.WriteLine("Item nr: " + itemNumber);
+            int x = mk[itemNumber].x_kratki;
+            int y = mk[itemNumber].y_kratki;
+            int id = mk[itemNumber].id;
+            bool haveParam = mk[itemNumber].maParametry;
+
+            if (mk[itemNumber].maParametry)
+            {
+                string param1 = mk[itemNumber].parametry.parametr1;
+                int param2 = mk[itemNumber].parametry.parametr2;
+                bool param3 = mk[itemNumber].parametry.parametr3;
+            }
+            // *** Koniec kodu by Nitro ***
+        }
+        static List<MojaKlasa> mk = new List<MojaKlasa>();
+        static void test()
+        {
+            ReadFromList(1);
+            if (param1 != "")
+            {
+                // some action
+            }
+        }
+    }
+    class KlasaID
+    {
+        public String parametr1 = "";
+        public int parametr2 = -1;
+        public bool parametr3 = false;
+    }
+    class MojaKlasa
+    {
+        public int x_kratki = -1;
+        public int y_kratki = -1;
+        public int id = -1;
+        public bool maParametry = false;
+        public KlasaID parametry = new KlasaID();
+    }
+}
